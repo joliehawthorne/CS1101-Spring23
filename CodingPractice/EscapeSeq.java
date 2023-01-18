e2ff18f8d2150ccf272cead52c962aae77124364
Gina Bai <rbai2@ncsu.edu>
2023-01-18 12:14:34 -0600

    Uploaded Lec4

---
+++ CodingPractice/EscapeSeq.java
@@ -0,0 +1,36 @@
+public class EscapeSeq {
+
+    public static void main(String[] args) {
+
+        // TODO: Write one print statement that reproduce the following pattern
+        /*
+            \ // -=- \ //
+         */
+
+        // TODO: Write one print statement that prints the following sentence:
+        /*
+            String is surrounded with double quotes ("")
+         */
+
+        // TODO: Write one print statement that prints the following sentence:
+        /*
+            char is surrounded with single quotes ('')
+         */
+
+        // TODO: Write one print statement that prints the following sentence:
+        /*
+            Hello
+            World
+         */
+
+        // TODO: Write one print statement that prints the following sentence:
+        /*
+            Hello   World
+         */
+
+        // TODO: Write one print statement that prints the following sentence:
+        /*
+            We need double backslashes \\ to print a single backslash \
+         */
+    }
+}
