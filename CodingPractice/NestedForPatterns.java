ffa3e13a531b549356c32333f9ef520c509bf716
Gina Bai <rbai2@ncsu.edu>
2023-02-20 12:21:48 -0600

    Uploaded Lec17

---
+++ CodingPractice/NestedForPatterns.java
@@ -0,0 +1,39 @@
+public class NestedForPatterns {
+    public static void main(String []args) {
+        
+        int row = 5;
+        int column = 5;
+
+        /* TODO: Reproduce the following patterns
+         
+         === Pattern 1 ===
+         *
+         **
+         ***
+         ****
+         *****
+
+         === Pattern 2 ===
+             1
+            2
+           3
+          4
+         5
+         
+         */
+        
+        System.out.println("=== Pattern 1 ===");
+        for () {
+            for () {
+                
+            }
+        }
+
+        System.out.println("\n=== Pattern 2 ===");
+        for () {
+            for () {
+                
+            }
+        }
+    }
+}
